99d61d05bf1250ce0e17dd23292efd9b663d5928
epinox0022 <fjhonnymark@gmail.com>
2025-05-12 14:48:25 +0800

    mark

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert("You Can't Do This Nigga!!");
+console.log("1 - 1");
+
+const x = 100;
+
+console.log(typeof x);
+console.error("alert");
+console.table({ name: "Jmark", email: "Jmark@gmail.com" });
+console.warn("Warning");
